4b426b4aac0fe5809ac826425904beee2f20c96f
XkaratistX <stomatolog456@gmail.com>
2021-03-16 11:51:19 +0100

    added slider new section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
 
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpead: 20000
+    });
+
 });
